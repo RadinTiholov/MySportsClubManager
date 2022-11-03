ae7d5d6949d4a01e218c3003c3404946a1399a5f
RadinTiholov <radin.tiholov@mail.bg>
2022-11-03 22:02:00 +0200

    Add validation file

---
+++ Data/MySportsClubManager.Data.Common/DataValidation.cs
@@ -0,0 +1,34 @@
+﻿namespace MySportsClubManager.Data.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class DataValidation
+    {
+        public static class Club
+        {
+            public const int NameMaxLength = 60;
+            public const int DescriptionMaxLength = 2000;
+            public const int AddressMaxLength = 500;
+        }
+
+        public static class Sport
+        {
+            public const int NameMaxLength = 60;
+            public const int DescriptionMaxLength = 2000;
+        }
+
+        public static class Creator
+        {
+            public const int NameMaxLength = 100;
+        }
+
+        public static class Country
+        {
+            public const int NameMaxLength = 50;
+        }
+    }
+}
