d9b10ac2f5ce0085c500cea327cc5ade7c09f1f3
RadinTiholov <radin.tiholov@mail.bg>
2022-11-13 19:55:27 +0200

    Add base PaginationViewModel

---
+++ Web/MySportsClubManager.Web.ViewModels/Base/PaginationViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace MySportsClubManager.Web.ViewModels.Base
+{
+    using System;
+
+    public class PaginationViewModel
+    {
+        public int PageNumber { get; set; }
+
+        public int SportsCount { get; set; }
+
+        public int ItemsPerPage { get; set; }
+
+        public int PreviousPageNumber => this.PageNumber - 1;
+
+        public int NextPageNumber => this.PageNumber + 1;
+
+        public bool HasPreviousPage => this.PageNumber > 1;
+
+        public bool HasNextPage => this.PageNumber < this.PagesCount;
+
+        public int PagesCount => (int)Math.Ceiling((double)this.SportsCount / this.ItemsPerPage);
+    }
+}
--- Web/MySportsClubManager.Web.ViewModels/Sport/SportListViewModel.cs
+++ Web/MySportsClubManager.Web.ViewModels/Sport/SportListViewModel.cs
@@ -3,24 +3,10 @@
     using System;
     using System.Collections.Generic;
 
-    public class SportListViewModel
+    using MySportsClubManager.Web.ViewModels.Base;
+
+    public class SportListViewModel : PaginationViewModel
     {
         public IEnumerable<SportInListViewModel> Sports { get; set; }
-
-        public int PageNumber { get; set; }
-
-        public int SportsCount { get; set; }
-
-        public int ItemsPerPage { get; set; }
-
-        public int PreviousPageNumber => this.PageNumber - 1;
-
-        public int NextPageNumber => this.PageNumber + 1;
-
-        public bool HasPreviousPage => this.PageNumber > 1;
-
-        public bool HasNextPage => this.PageNumber < this.PagesCount;
-
-        public int PagesCount => (int)Math.Ceiling((double)this.SportsCount / this.ItemsPerPage);
     }
 }
